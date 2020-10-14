ca99087305c448d25b502b7443a5a9f3c6869f40
Kalle R <kr@impera.se>
2020-10-14 19:08:03 +0200

    kallesurf: Some styling + finish locale-support

--- vhosts/kalle-surf/routes/index.js
+++ vhosts/kalle-surf/routes/index.js
@@ -8,13 +8,19 @@ const cache = require('express-redis-cache')({
 });
 
 const ensureHttps = require('@lib/middleware/ensure-https');
-
+const parseFivebyfive = require('@lib/middleware/parse-fivebyfive');
 const router = express.Router();
 
 router
     .use(ensureHttps)
+    .use(parseFivebyfive)
 
-    .get('/', cache.route(), async (req, res) => res.render('index'))
+    .get('/',  async (req, res) => res.render('index'))
+
+    .get('/l/:lang', (req, res) => {
+        res.cookie('language', req.params.lang, { maxAge: 900000, httpOnly: true });
+        res.redirect(req.header('Referer') || '/');
+    })
 ;
 
 module.exports = router;
